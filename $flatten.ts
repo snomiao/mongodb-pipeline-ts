28cadc68cf1812533c8c3b3a6d243f42e0792b19
snomiao <snomiao@gmail.com>
2024-06-15 15:39:41 +0000

    chore(main): stage
    stage

---
+++ $flatten.ts
@@ -0,0 +1,34 @@
+import { type Document, type Filter } from "mongodb";
+import { fromPairs, toPairs } from "rambda";
+/**
+ * This function flattens the filter object, used for mongodb partial queries.
+ * otherwise, mongodb will match the nested object as a whole.
+ * @example
+ * ```ts
+ * $flatten({ a: { b: 1 } }) // { "a.b": 1 }
+ *
+ * coll.find($flatten({ a: { b: 1 } })) // coll.find({ "a.b": 1 })
+ * // this will match the document { a: { b: 1 } } and { a: { b: 1, c: 2 } }
+ * // but not { a: { b: 2 } }
+ *
+ * // by comparison:
+ * coll.find({ a: { b: 1 } }) // coll.find({ a: { b: 1 } })
+ * // this will only match the document { a: { b: 1 } }
+ * // but not { a: { b: 1, c: 2 } } or { a: { b: 2 } }
+ * ```
+ */
+export function $flatten<TSchema extends Document>(filter: Filter<TSchema>): Filter<TSchema> {
+  const v = filter as any;
+  if (typeof v !== "object" || !(v instanceof Object)) return v;
+  if (v instanceof Date) return v;
+  if (Array.isArray(v)) return v.map($flatten) as any;
+  return fromPairs(
+    toPairs(v).flatMap(([k, v]) => {
+      if (typeof v !== "object" || !(v instanceof Object)) return [[k, v]];
+      if (k.startsWith("$")) return [[k, $flatten(v)]];
+      if (Object.keys(v).some((kk) => kk.startsWith("$"))) return [[k, $flatten(v)]];
+      // TODO: optimize this
+      return toPairs($flatten(fromPairs(toPairs(v).map(([kk, vv]) => [`${k}.${kk}`, vv]))));
+    }, v) as any,
+  );
+}
---
+++ index.ts
@@ -0,0 +1,79 @@
+import { Collection, FindCursor, type Document, type Filter } from "mongodb";
+import type { FieldArrayPath, FieldArrayPathValue, FieldPath, FieldPathValue, FieldValue } from "react-hook-form";
+import { $flatten } from "./$flatten";
+type NODOT<T extends string> = T extends `${infer K}.${infer _}` ? never : T;
+// k in string extends FieldArrayPath<Filter<TSchema>>
+// type SetArrayPath<TSchema extends Document> =  `${FAP}.$.${FieldPath<Filter<TSchema>, FieldArrayPathValue<TSchema, FAP>> }`
+type $Path<TSchema extends Document> = `$${FieldPath<TSchema>}`;
+// type $set<TSchema> = Record<NODOT<keyof TSchema>, FieldValue<TSchema>>;
+// type ArrayKey<T, K> = T[K] extends ReadonlyArray ? K : never;
+// type FieldArrayKey<T> = FieldArray
+export type UnwrapArrayValue<V> = NonNullable<V> extends any[] ? NonNullable<V>[number] : V;
+export declare type $Set<TSchema extends Document> = {
+  [P in keyof TSchema]?:
+    | (TSchema[P] extends Record<string, any> ? $Set<UnwrapArrayValue<TSchema[P]>> : TSchema[P])
+    | $Path<TSchema>;
+} & Record<string, any>;
+export type $SetResult<TSchema extends Document, Set extends $Set<TSchema>> = {
+  [P in keyof TSchema]?: Set[P] extends `$${infer K extends FieldPath<TSchema>}`
+    ? FieldPathValue<TSchema, K>
+    : Set[P] extends Record<string, any>
+      ? $SetResult<FieldPathValue<Set[P], FieldPath<Set[P]>>, Set[P]>
+      : Set[P];
+};
+export type $Project<TSchema extends Document> = {
+  [P in FieldPath<TSchema>]?: 1 | 0 | $Path<TSchema>;
+};
+export type $ProjectResult<TSchema extends Document, Project extends $Project<TSchema>> = {
+  [P in FieldPath<TSchema>]?: Project[P] extends 1
+    ? TSchema[P]
+    : Project[P] extends 0
+      ? never
+      : Project[P] extends `$${infer K extends FieldPath<TSchema>}`
+        ? FieldPathValue<TSchema, K>
+        : never;
+};
+
+export function $pipeline<TSchema extends Document>(coll?: Collection<TSchema>, pipeline = [] as readonly Document[]) {
+  const _coll = coll as any;
+  return {
+    aggregate() {
+      if (!coll) throw new Error("Collection not provided");
+      return coll.aggregate([...pipeline]) as unknown as FindCursor<TSchema>;
+    },
+    _peek(fn = <T>(e: Readonly<T>): T => e) {
+      return $pipeline<TSchema>(_coll, fn(pipeline));
+    },
+    project<P extends $Project<TSchema>>($project: P) {
+      return $pipeline<$ProjectResult<TSchema, P>>(_coll, [...pipeline, { $project }]);
+    },
+    set<T extends $Set<TSchema>>($set: T) {
+      return $pipeline<TSchema & T>(_coll, [...pipeline, { $set }]);
+    },
+    setFlat<
+      T extends {
+        [k in FieldPath<TSchema>]: FieldValue<TSchema> | `$${FieldPath<TSchema>}`;
+      } & Record<`${FieldPath<TSchema>}.${string}`, any> &
+        Record<string, any>,
+    >($set: T) {
+      return $pipeline<TSchema & T>(_coll, [...pipeline, { $set }]);
+    },
+    setFlatten<T extends $Set<TSchema>>($set: T) {
+      return $pipeline<$SetResult<TSchema, T>>(_coll, [...pipeline, { $set: $flatten($set) }]);
+    },
+    match(filter: Filter<TSchema>) {
+      return $pipeline<TSchema>(_coll, [...pipeline, { $match: filter }]);
+    },
+    matchFlatten(filter: Filter<TSchema>) {
+      return $pipeline<TSchema>(_coll, [...pipeline, { $match: $flatten(filter) }]);
+    },
+    unwind<Path extends FieldArrayPath<TSchema>>($unwind: `$${Path}`) {
+      type Unwinded = { [k in Path]: NonNullable<FieldArrayPathValue<TSchema, Path>>[number] };
+      return $pipeline<Unwinded>(_coll, [...pipeline, { $unwind }]);
+    },
+    replaceRoot<Path extends FieldPath<TSchema>>(opt: { newRoot: `$${Path}` }) {
+      type Replaced = NonNullable<FieldPathValue<TSchema, Path> | TSchema[Path]>;
+      return $pipeline<Replaced>(_coll, [...pipeline, { $replaceRoot: opt }]);
+    },
+  };
+}
