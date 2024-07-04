71611887bf67ac97e7c5d57d9d8cb2cdf87dfd2d
snomiao <snomiao@gmail.com>
2024-07-04 18:14:23 +0900

    fix(unwind): use FieldPathByValue
    use FieldPathByValue

--- $pipeline.ts
+++ $pipeline.ts
@@ -6,7 +6,7 @@ import {
   type Filter,
   type IndexSpecification,
 } from "mongodb";
-import type { FieldArrayPath, FieldArrayPathValue, FieldPath, FieldPathValue } from "react-hook-form";
+import type { FieldPath, FieldPathByValue, FieldPathValue } from "react-hook-form";
 import type { Update as UpdateAt } from "ts-toolbelt/out/Object/P/Update";
 import type { Split } from "ts-toolbelt/out/String/Split";
 import type { AllPath } from "./AllPath";
@@ -188,9 +188,9 @@ type Stages<S extends Document> = {
    * $unset is an alias for $project stage that removes fields. */
   unset<I extends string | string[]>(i: I): StageBuilder<Omit<S, I extends any[] ? I[number] : I>>;
   /** Deconstructs an array field from the input documents to output a document for each element. Each output document replaces the array with an element value. For each input document, outputs n documents where n is the number of array elements and can be zero for an empty array. */
-  unwind: <P extends FieldArrayPath<S>>(
+  unwind: <P extends FieldPathByValue<S, ReadonlyArray<any>>>(
     i: `$${P}`,
-  ) => StageBuilder<UpdateAt<S, Split<P, ".">, FieldArrayPathValue<S, P>[number]>>;
+  ) => StageBuilder<UpdateAt<S, Split<P, ".">, FieldPathValue<S, P>[number]>>;
   /** Performs an ANN search on a vector in the specified field of an Atlas collection.
    * New in version 7.0.2. */
   vectorSearch<I extends Document>(i: I): StageBuilder<S>;
